8159f126afd43d5a45aaf864eef7079e3cac88d8
Sleeyax <yourd3veloper@gmail.com>
2021-01-24 23:20:03 +0100

    Fix executor test

--- CronetSharp.Tests/ExecutorTest.cs
+++ CronetSharp.Tests/ExecutorTest.cs
@@ -3,18 +3,18 @@
 namespace CronetSharp.Tests
 {
     [TestFixture]
-    public class ExecutorTest
+    public class ExecutorTest : SetupCronet
     {
+        [Test]
         public void TestCanExecuteRunnable()
         {
             bool ran = false;
-            using var runnable = new Runnable(() => ran = true);
             using var executor = new Executor();
+            // NOTE: we don't use 'using' here, because runnable is already disposed by this executor!
+            var runnable = new Runnable(() => ran = true);
             Assert.IsFalse(ran);
             executor.Execute(runnable);
             Assert.IsTrue(ran);
         }
-        
-        
     }
 }
