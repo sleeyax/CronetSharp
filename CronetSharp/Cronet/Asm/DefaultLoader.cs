c7fa987f544a94c6d896e9b68dd7424e95759523
Sleeyax <yourd3veloper@gmail.com>
2021-01-24 16:26:49 +0100

    Oops :facepalm:

---
+++ CronetSharp/Cronet/Asm/DefaultLoader.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace CronetSharp.Cronet.Asm
+{
+    public class CronetLoader : ILoader
+    {
+        internal const string Dll = "cronet.dll";
+        
+        [DllImport("kernel32.dll")]
+        protected static extern IntPtr LoadLibrary(string dllToLoad);
+
+        /// <summary>
+        /// Loads the cronet dll into the current process
+        /// </summary>
+        /// <param name="path">relative path to the dll to load</param>
+        public virtual void Load(string path)
+        {
+            var localPath = new Uri(typeof(Engine).Assembly.EscapedCodeBase).LocalPath;
+            var dir = Path.GetDirectoryName(localPath);
+            var platform = Environment.Is64BitProcess ? "Win64" : "Win32";
+            var asmPath = Path.Combine(dir, path, platform, Dll);
+            
+            #if DEBUG
+                Console.WriteLine($"Loading {asmPath}");
+            #endif
+            
+            LoadLibrary(asmPath);
+        }
+    }
+}
---
+++ CronetSharp/Cronet/Asm/ILoader.cs
@@ -0,0 +1,11 @@
+﻿namespace CronetSharp.Cronet.Asm
+{
+    public interface ILoader
+    {
+       /// <summary>
+       /// Load the cronet dll into the current process
+       /// </summary>
+       /// <param name="path">relative directory to the dll</param>
+        void Load(string path);
+    }
+}
