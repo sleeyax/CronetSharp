c7080e1bc56629bd2cce2944b2bc0c55b8b92a33
Sleeyax <yourd3veloper@gmail.com>
2021-01-24 23:28:17 +0100

    Add http header test

---
+++ CronetSharp.Tests/HttpHeaderTest.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace CronetSharp.Tests
+{
+    [TestFixture]
+    public class HttpHeaderTest : SetupCronet
+    {
+        [Test]
+        public void TestHttpHeader()
+        {
+            using var header = new HttpHeader("foo", "bar");
+            Assert.AreEqual("foo", header.Name);
+            Assert.AreEqual("bar", header.Value);
+            header.Value = "baz";
+            Assert.AreEqual("foo", header.Name);
+            Assert.AreEqual("baz", header.Value);
+            header.Dispose();
+        }
+    }
+}
